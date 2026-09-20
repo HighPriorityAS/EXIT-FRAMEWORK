468e2817ad0f2443af00e6aef16bef99d91a9fc4
HighPriorityAS <atleskarstein6@gmail.com>
2026-09-20 18:21:34 +0200

    Include Exit mark in release QA manifest

--- validate.py
+++ validate.py
@@ -27,6 +27,7 @@ CORE_MANIFEST_FILES = (
     'exit-state.js',
     'account.js',
     'CNAME',
+    'assets/exit-mark.svg',
     'assets/exit-framework-hero-source.jpg',
     'assets/ibm-plex-mono-regular.ttf',
 )
